64627fab49dd2a6cba70fe53bc0ebabcc492c801
Marcus Erick <marcushp1@hotmail.com>
2024-04-13 21:38:46 -0600

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Marcus Erick Hinojosa Piña on 13/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
